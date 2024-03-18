176150267bd5fe72a1d870fa7fc7a92543339c77
Patrick Moscova <patrick_moscova@Patricks-MacBook-Pro.local>
2024-03-18 15:33:58 -0400

    I'm tired of this grandpa

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func fibbonacci(_ n:Int) -> Int {
+    if n <= 1{
+        return n
+    }else {
+        return fibbonacci( n - 1) + fibbonacci( n - 2)
+    }
+}
+
+func printFib(upTo n: Int ) {
+    var seq = [Int]()
+    
+    for i in 0..<n {
+        seq.append(fibbonacci(i))
+    }
+    print(seq)
+}
+
+printFib(upTo: 10)
